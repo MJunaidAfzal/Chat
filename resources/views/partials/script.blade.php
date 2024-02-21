7748860632c16bd369e682218d3fa48258252c35
Your NameMJunaidAfzal <mjunaidafzal395@gmail.com>
2024-02-21 23:48:16 +0500

    junaid

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -17,21 +17,22 @@
 
    <script src="{{asset('assets/js/app.js')}}"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-  <script>
-    toastr.options = {
-            "progressBar" : true,
-            "closeButton" : true,
-    }
-    @if(Session::has('success'))
-        toastr.success("{{ session('success') }}")
-    @endif
-  </script>
-  <script>
-    toastr.options = {
-            "progressBar" : true,
-            "closeButton" : true,
-    }
-    @if(Session::has('error'))
-        toastr.error("{{ session('error') }}")
-    @endif
-  </script>
+   <script>
+     toastr.options = {
+             "progressBar" : true,
+             "closeButton" : true,
+     }
+     @if(Session::has('success'))
+         toastr.success("{{ session('success') }}")
+     @endif
+   </script>
+   <script>
+     toastr.options = {
+             "progressBar" : true,
+             "closeButton" : true,
+     }
+     @if(Session::has('error'))
+         toastr.error("{{ session('error') }}")
+     @endif
+   </script>
+
